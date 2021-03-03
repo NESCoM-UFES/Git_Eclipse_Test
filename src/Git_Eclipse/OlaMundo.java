b734aba1292f3bab3e21366dbcbc7172b7c05ee3
Leandro Costalonga <llcostalonga@gmail.com>
2021-03-03 20:11:42 -0300

    parte 6

--- src/Git_Eclipse/OlaMundo.java
+++ src/Git_Eclipse/OlaMundo.java
@@ -3,14 +3,14 @@ package Git_Eclipse;
 public class OlaMundo {
 
 	public static String getMessage() {
-		return "Mensagem de saudação do Leandro";
+		return "Mensagem de saudaÃ§Ã£o do Leandro";
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(getMessage());
-		System.out.println("Altera��o Local 2");
+		System.out.println("Alteração Local 2");
 		System.out.println("experiment");
-		System.out.println("modificao no site - parte 5");
+		System.out.println("modificao no site - parte 6");
 		System.out.println("Alteracao muito muito importante");
 		
 	}

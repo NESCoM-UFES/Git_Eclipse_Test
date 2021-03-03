b69823626727147abf3d24033240fbf4e3dde2ac
Leandro <llcostalonga@gmail.com>
2021-03-03 20:22:54 -0300

    novo metodo e parte 6

--- src/Git_Eclipse/OlaMundo.java
+++ src/Git_Eclipse/OlaMundo.java
@@ -3,7 +3,7 @@ package Git_Eclipse;
 public class OlaMundo {
 
 	public static String getMessage() {
-		return "Mensagem de saudação do Leandro";
+		return "Mensagem de saudaÃ§Ã£o do Leandro";
 	}
 	
 	public static String novoMetodo() {
@@ -12,9 +12,9 @@ public class OlaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(getMessage());
-		System.out.println("Altera��o Local 2");
+		System.out.println("Alteração Local 2");
 		System.out.println("experiment");
-		System.out.println("modificao no site - parte 5");
+		System.out.println("modificao no site - parte 6");
 		System.out.println("Alteracao muito muito importante");
 		
 	}

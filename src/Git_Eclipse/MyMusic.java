4a4af77b7e80e0c27d95ed692c7d7dbeb341803a
Leandro <llcostalonga@gmail.com>
2021-03-13 06:33:38 -0300

    teste fork

--- src/Git_Eclipse/MyMusic.java
+++ src/Git_Eclipse/MyMusic.java
@@ -5,9 +5,7 @@ public class MyMusic {
 
 	public static void main(String[] args) {
 
-		System.out.println("Faz algo novo");
-		System.out.println("tocando musiquinha bacana");
-		System.out.println("tocando musiquinha bacana 2");
+		//Fork
 	}
 
 }
